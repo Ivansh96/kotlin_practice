38a7642718a0971a5cf9df5830244d4d77dc9514
Ivansh96 <isavsin5@gmail.com>
2024-05-03 16:30:15 +0700

    budget api, record and service edition

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,6 +2,12 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import mobi.sevenwinds.app.author.AuthorTable
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
+import org.jetbrains.exposed.sql.andWhere
+import org.jetbrains.exposed.sql.lowerCase
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -13,28 +19,39 @@ object BudgetService {
                 this.month = body.month
                 this.amount = body.amount
                 this.type = body.type
+                this.author = body.author?.let { EntityID(it, AuthorTable) }
             }
 
             return@transaction entity.toResponse()
         }
     }
 
-    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
-        transaction {
-            val query = BudgetTable
-                .select { BudgetTable.year eq param.year }
-                .limit(param.limit, param.offset)
+    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse =
+        withContext(Dispatchers.IO) {
+            transaction {
+                val query = BudgetTable
+                    .join(AuthorTable, JoinType.INNER, BudgetTable.author, AuthorTable.id)
+                    .select { BudgetTable.year eq param.year }
+                    .limit(param.limit, param.offset)
+                if (!param.authorNameFilter.isNullOrBlank()) {
+                    query.andWhere {
+                        AuthorTable.fullName.lowerCase() like
+                                "%${param.authorNameFilter.toLowerCase()}%"
+                    }
+                }
 
-            val total = query.count()
-            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
+                val total = query.count()
+                val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
 
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
 
-            return@transaction BudgetYearStatsResponse(
-                total = total,
-                totalByType = sumByType,
-                items = data
-            )
+                val sumByType =
+                    data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+
+                return@transaction BudgetYearStatsResponse(
+                    total = total,
+                    totalByType = sumByType,
+                    items = data,
+                )
+            }
         }
-    }
 }
--- src/main/java/mobi/sevenwinds/app/budget/BudgetTable.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetTable.kt
@@ -1,15 +1,21 @@
 package mobi.sevenwinds.app.budget
 
+import mobi.sevenwinds.app.author.AuthorEntity
+import mobi.sevenwinds.app.author.AuthorTable
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object BudgetTable : IntIdTable("budget") {
     val year = integer("year")
     val month = integer("month")
     val amount = integer("amount")
     val type = enumerationByName("type", 100, BudgetType::class)
+    val author = optReference("author_id", AuthorTable.id)
 }
 
 class BudgetEntity(id: EntityID<Int>) : IntEntity(id) {
@@ -19,8 +25,24 @@ class BudgetEntity(id: EntityID<Int>) : IntEntity(id) {
     var month by BudgetTable.month
     var amount by BudgetTable.amount
     var type by BudgetTable.type
+    var author by BudgetTable.author
 
     fun toResponse(): BudgetRecord {
-        return BudgetRecord(year, month, amount, type)
+        val recordDate = author?.let {
+            transaction {
+                val query = AuthorTable
+                    .select { AuthorTable.id eq author?.value }
+                return@transaction AuthorEntity.wrapRow(query.first()).creationDate
+            }
+        }
+
+        val authorFullName = author?.let {
+            transaction {
+                val query = AuthorTable
+                    .select { AuthorTable.id eq author?.value }
+                return@transaction AuthorEntity.wrapRow(query.first()).fullName
+            }
+        }
+        return BudgetRecord(year, month, amount, type, author?.value, authorFullName, recordDate)
     }
 }
